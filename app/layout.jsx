15525b91d45e86b3afb2b3c136984dcf2dba0b19
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-20 20:28:16 -0300

    chore: configure custom fonts, TailwindCSS theme, and global styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "300", "400", "700"],
+  variable: "--font-lato",
 });
 
 export const metadata = {
@@ -18,9 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.variable} antialiased`}
       >
         {children}
       </body>
