5256cc285c923782cc2c1d35bce55c0a77eef6e0
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-22 21:52:16 -0300

    chore: rename Photo.jsx to PhotoBanner.jsx and optimize for banner-specific images

--- components/PhotoBanner.jsx
+++ components/PhotoBanner.jsx
@@ -1,22 +1,21 @@
 "use client";
 
-import { motion } from "framer-motion";
 import Image from "next/image";
 
-const Photo = ({ src, alt }) => {
+const PhotoBanner = ({ src, alt }) => {
   return (
     <div className="w-[256px] xl:w-[448px] h-[256px] xl:h-[448px] m-12 relative flex items-center justify-center">
       <Image
         src={src}
         priority
         quality={100}
-        fill
         alt={alt}
-        className="rounded-[80px_4px] xl:rounded-[144px_4px] object-contain shadow-[20px_-20px_0px_rgba(141,142,122,0.8)]"
-        sizes="256px"
+        width={384}
+        height={384}
+        className="rounded-[80px_4px] xl:rounded-[128px_4px] object-contain shadow-[20px_-20px_0px_rgba(141,142,122,0.8)]"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Photo;
+export default PhotoBanner;
