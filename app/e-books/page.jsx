b3107b03f6946b016010fc752c1c8da142c8d425
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-31 23:00:09 -0300

    eat: add Ebook page

--- app/e-books/page.jsx
+++ app/e-books/page.jsx
@@ -1,5 +1,14 @@
+import Banner from "@/components/Banner";
+import EbookSection from "@/components/home/EbookSection";
+
 const Ebooks = () => {
-  return <div>e-books page</div>
+  return <section>
+    <Banner
+      title="E-books por Edvânia Soares"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae nunc. Nulla facilisi."
+    />
+    <EbookSection />
+  </section>
 }
 
 export default Ebooks;
