41d6a57abb231e5a94341bfdf415e468cbe3d380
Ariel Spencer <arielspencer.tech@gmail.com>
2025-02-10 22:58:32 -0300

    feat: remove YouTube section from home page and delete component

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,6 @@ import ContactSection from "@/components/home/ContactSection";
 import EbookSection from "@/components/home/EbookSection";
 import MediaSection from "@/components/home/MediaSection";
 import ServicesSection from "@/components/home/ServicesSection";
-import YouTubeSection from "@/components/home/YouTubeSection";
 
 const Home = () => {
   return (
@@ -14,7 +13,6 @@ const Home = () => {
       <ServicesSection />
       <AboutSection />
       <MediaSection />
-      <YouTubeSection />
       <EbookSection />
       <BlogSection />
       <ContactSection />
